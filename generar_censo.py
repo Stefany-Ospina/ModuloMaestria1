b1aefdc9ba2717b51d47e5f34fbf094bbb49c01e
Stefany-Ospina <158113966+Stefany-Ospina@users.noreply.github.com>
2024-02-01 02:25:54 +0000

    app flask

--- generar_censo.py
+++ generar_censo.py
@@ -2,40 +2,45 @@ import random
 import sqlite3
 
 def generar_censo(cantidad):
-  censo=[]
-  alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  numero=0
+  censo = []
+  alfabeto = "ABCDFDEDASDFGEQRWETYUIOPLKJDHMMAEAAOIOUE"
+  numero = 0
 
-  for _i in range(cantidad):
-      aumento =random.randint(1,2)
+  for i in range(cantidad):
+      aumento = random.randint(1, 2)
       numero += aumento
 
-      letras = random.sample(alfabeto,5)
-      nombre = "".join(letras)#unir las letras aleatorias
+      letras = random.sample(alfabeto, 5)
+      nombre = "".join(letras)
 
-      edad = random.randint(18,99)
-
+      edad = random.randint(18, 99)
       impuestos = random.choice((True, True, True, False))
 
       censo.append([numero, nombre, edad, impuestos])
+
   return censo
 
 def main():
-  cantidad_registros=1050000
-  censo=generar_censo(cantidad_registros)
-#conexion a la base de datos
-  conn=sqlite3.connect("censo.db")#sino tiene la base de datos la crea
-  c=conn.cursor()#se crea cursor para ejecutar las sentencias sql
-#crear una tabla sino existe
-  c.execute('''
-          CREATE TABLE IF NOT EXISTS censo(
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          numero INTEGER,
-          nombres TEXT,
-          edad INTEGER,
-          impuestos BOOLEAN)
-          ''')
-  c.executemany('INSERT INTO censo (numero,nombre, edad, impuestos) VALUES(?,?,?,?)', censo)
-  conn.commit()
-  conn.close()
-  
+  cantidad_registros = 1050000
+  censo = generar_censo(cantidad_registros)
+
+  # Conexion a la base de datos
+  conn = sqlite3.connect("censo.db") # Si no se tiene la base de datos, se crea
+  c = conn.cursor() # Se crea un cursor para ejecutar las sentencias SQL
+
+  # Crear una tabla si no existe
+  c.execute('''CREATE TABLE IF NOT EXISTS censo (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              numero INTEGER,
+              nombre TEXT,
+              edad INTEGER,
+              impuestos BOOLEAN)''')
+  # Insertar los registros en la tabla
+  c.executemany('INSERT INTO censo (numero, nombre, edad, impuestos) VALUES (?, ?, ?, ?)', censo)
+  conn.commit() # Guardar los cambios en la base de datos
+  conn.close() # Cerrar la conexión a la base de datos
+
+  print("Censo creado con exito")
+
+if __name__ == "__main__":
+  main()
